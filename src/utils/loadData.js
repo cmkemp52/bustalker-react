b59afca763fc5e5289d73a5a8032e3410aeaf716
Christopher Kemp <cmkemp52@gmail.com>
2019-11-07 09:44:42 -0500

    added cors proxy

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -1,5 +1,5 @@
 export const loadData = async(url) =>{
-    const response = await fetch(url);
+    const response = await fetch('https://my-little-cors-proxy.herokuapp.com/' +url);
     const data = await response.json();
     return data;
 };
