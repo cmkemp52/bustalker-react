c9e65b3860d495f4a0a032eeae6e2ac40db510ab
Christopher Kemp <cmkemp52@gmail.com>
2019-11-07 09:39:33 -0500

    added busTracker framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {BusTracker} from './components/busTracker';
 
 function App() {
   return (
     <div className="App">
-      
+      <BusTracker />
     </div>
   );
 }
